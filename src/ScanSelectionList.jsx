615d1e3f40193dec23562c5ff79683ea4ae85b79
Nader Morshed <morshed.nader@gmail.com>
2016-08-12 17:24:11 -0400

    Fixed expanding tree with arrow keys
    Linted module a bit

--- src/ScanSelectionList.jsx
+++ src/ScanSelectionList.jsx
@@ -4,171 +4,260 @@ hotkey.activate();
 var PTMPlacementListItem = React.createClass({
   update: function() {
     // console.log(this.props.proteinId, this.props.peptideId, this.props.scanId, this.props.ptmPlacement.id)
-    this.props.update(this.props.proteinId,
-                      this.props.peptideId,
-                      this.props.scanId,
-                      this.props.ptmPlacement.id)
+    this.props.update(
+      this.props.proteinId,
+      this.props.peptideId,
+      this.props.scanId,
+      this.props.ptmPlacement.id
+    )
   },
-  render: function(){
-    var selected = (this.props.selectedProtein == this.props.proteinId &&
-                    this.props.selectedPeptide == this.props.peptideId &&
-                    this.props.selectedScan == this.props.scanId &&
-                    this.props.selectedPTMPlacement == this.props.ptmPlacement.id)
-    if (this.props.choice == 'accept'){
-      return <li className='accept' onClick={this.update}><span className={selected ? 'selectedListItem' : 'unselectedListItem'}>{this.props.ptmPlacement.name}</span></li>
-    } else if (this.props.choice == 'maybe'){
-      return <li className='maybe' onClick={this.update}><span className={selected ? 'selectedListItem' : 'unselectedListItem'}>{this.props.ptmPlacement.name}</span></li>
-    } else if (this.props.choice == 'reject'){
-      return <li className='reject' onClick={this.update}><span className={selected ? 'selectedListItem' : 'unselectedListItem'}>{this.props.ptmPlacement.name}</span></li>
+
+  render: function() {
+    var selected = (
+      this.props.selectedProtein == this.props.proteinId &&
+      this.props.selectedPeptide == this.props.peptideId &&
+      this.props.selectedScan == this.props.scanId &&
+      this.props.selectedPTMPlacement == this.props.ptmPlacement.id
+    )
+
+    if (this.props.choice == 'accept') {
+      return <li className='accept' onClick={this.update}>
+        <span className={selected ? 'selectedListItem' : 'unselectedListItem'}>{this.props.ptmPlacement.name}</span>
+      </li>
+    } else if (this.props.choice == 'maybe') {
+      return <li className='maybe' onClick={this.update}>
+        <span className={selected ? 'selectedListItem' : 'unselectedListItem'}>{this.props.ptmPlacement.name}</span>
+      </li>
+    } else if (this.props.choice == 'reject') {
+      return <li className='reject' onClick={this.update}>
+        <span className={selected ? 'selectedListItem' : 'unselectedListItem'}>{this.props.ptmPlacement.name}</span>
+      </li>
     } else {
-      return <li className='undecided' onClick={this.update}><span className={selected ? 'selectedListItem' : 'unselectedListItem'}>{this.props.ptmPlacement.name}</span></li>
+      return <li className='undecided' onClick={this.update}>
+        <span className={selected ? 'selectedListItem' : 'unselectedListItem'}>{this.props.ptmPlacement.name}</span>
+      </li>
     }
   }
 });
 
 var ScanNumberListItem = React.createClass({
-  getInitialState: function(){
-    return {open: false}
+  getInitialState: function() {
+    return {
+      open: this.childSelected()
+    }
   },
+
+  childSelected: function() {
+    return (
+      this.props.selectedProtein == this.props.proteinId &&
+      this.props.selectedPeptide == this.props.peptideId &&
+      this.props.selectedScan == this.props.scan.scanId &&
+      this.props.selectedPTMPlacement != null
+    );
+  },
+
   toggle: function() {
     this.setState({open: !this.state.open})
-    this.props.update(this.props.proteinId, this.props.peptideId, this.props.scan.scanId, null)
+    this.props.update(
+      this.props.proteinId,
+      this.props.peptideId,
+      this.props.scan.scanId,
+      null
+    )
   },
+
   render: function() {
-    var selected = (this.props.selectedProtein == this.props.proteinId &&
-                    this.props.selectedPeptide == this.props.peptideId &&
-                    this.props.selectedScan == this.props.scan.scanId &&
-                    this.props.selectedPTMPlacement == null)
-    if (this.state.open){
+    var selected = (
+      this.props.selectedProtein == this.props.proteinId &&
+      this.props.selectedPeptide == this.props.peptideId &&
+      this.props.selectedScan == this.props.scan.scanId &&
+      this.props.selectedPTMPlacement == null
+    )
+
+    if (this.state.open || this.childSelected()) {
       return (
         <div>
-          <li className="scan" onClick={this.toggle}><span className={selected ? 'selectedListItem' : 'unselectedListItem'}>Scan: {this.props.scan.scanNumber}</span></li>
+          <li className="scan" onClick={this.toggle}>
+            <span className={selected ? 'selectedListItem' : 'unselectedListItem'}>Scan: {this.props.scan.scanNumber}</span>
+          </li>
           <ul>
-            {this.props.ptmPlacements.map(function(ptmPlacement, i){
-              return ( <PTMPlacementListItem key={ptmPlacement.id}
-                                             choice={this.props.scan.choiceData[i].state}
-                                             proteinId={this.props.proteinId}
-                                             peptideId={this.props.peptideId}
-                                             scanId={this.props.scan.scanId}
-                                             ptmPlacement={ptmPlacement}
-                                             update={this.props.update}
-                                             selectedProtein={this.props.selectedProtein}
-                                             selectedPeptide={this.props.selectedPeptide}
-                                             selectedScan={this.props.selectedScan}
-                                             selectedPTMPlacement={this.props.selectedPTMPlacement}/>
-
-
-                     )
-            }.bind(this))}
+            {
+              this.props.ptmPlacements.map(function(ptmPlacement, i) {
+                return (
+                  <PTMPlacementListItem
+                    key={ptmPlacement.id}
+                    choice={this.props.scan.choiceData[i].state}
+                    proteinId={this.props.proteinId}
+                    peptideId={this.props.peptideId}
+                    scanId={this.props.scan.scanId}
+                    ptmPlacement={ptmPlacement}
+                    update={this.props.update}
+                    selectedProtein={this.props.selectedProtein}
+                    selectedPeptide={this.props.selectedPeptide}
+                    selectedScan={this.props.selectedScan}
+                    selectedPTMPlacement={this.props.selectedPTMPlacement}/>
+                )
+              }.bind(this))
+            }
           </ul>
         </div>
       )
     } else {
       return (
-        <li className="scan" onClick={this.toggle}><span className={selected ? 'selectedListItem' : 'unselectedListItem'}>Scan: {this.props.scan.scanNumber}</span></li>
+        <li className="scan" onClick={this.toggle}>
+          <span className={selected ? 'selectedListItem' : 'unselectedListItem'}>Scan: {this.props.scan.scanNumber}</span>
+        </li>
       )
     }
   }
 });
 
 var PeptideListItem = React.createClass({
-  getInitialState: function(){
-    return {open: false}
+  getInitialState: function() {
+    return {
+      open: this.childSelected()
+    }
   },
-  toggle: function(){
+
+  childSelected: function() {
+    return (
+      this.props.selectedProtein == this.props.proteinId &&
+      this.props.selectedPeptide == this.props.peptideId &&
+      this.props.selectedScan != null
+    )
+  },
+
+  toggle: function() {
     this.setState({open: !this.state.open})
     this.props.update(this.props.proteinId, this.props.peptideId, null, null)
   },
+
   render: function() {
-    var selected = (this.props.selectedProtein == this.props.proteinId &&
-                    this.props.selectedPeptide == this.props.peptideId &&
-                    this.props.selectedScan == null &&
-                    this.props.selectedPTMPlacement == null)
+    var selected = (
+      this.props.selectedProtein == this.props.proteinId &&
+      this.props.selectedPeptide == this.props.peptideId &&
+      this.props.selectedScan == null &&
+      this.props.selectedPTMPlacement == null
+    )
     var peptideModificationState = this.props.peptideData.modificationStates[this.props.modificationStateId]
 
     var sequenceName = this.props.peptideData.peptideSequence + peptideModificationState.modDesc
-    if (this.state.open){
+    if (this.state.open || this.childSelected()) {
       return (
         <div>
-        <li className="peptide" onClick={this.toggle}><span className={selected ? 'selectedListItem' : 'unselectedListItem'}>{sequenceName}</span></li>
-        <ul>
-          {this.props.scans.map(function(scan){
-            return ( <ScanNumberListItem key={scan.scanId}
-                                         proteinId={this.props.proteinId}
-                                         peptideId={this.props.peptideId}
-                                         scan={scan}
-                                         ptmPlacements={peptideModificationState.mods}
-                                         update={this.props.update}
-                                         selectedProtein={this.props.selectedProtein}
-                                         selectedPeptide={this.props.selectedPeptide}
-                                         selectedScan={this.props.selectedScan}
-                                         selectedPTMPlacement={this.props.selectedPTMPlacement}/>
-                   )
-          }.bind(this))}
-        </ul>
+          <li className="peptide" onClick={this.toggle}>
+            <span className={selected ? 'selectedListItem' : 'unselectedListItem'}>{sequenceName}</span>
+          </li>
+          <ul>
+            {
+              this.props.scans.map(function(scan) {
+                return (
+                  <ScanNumberListItem
+                    key={scan.scanId}
+                    proteinId={this.props.proteinId}
+                    peptideId={this.props.peptideId}
+                    scan={scan}
+                    ptmPlacements={peptideModificationState.mods}
+                    update={this.props.update}
+                    selectedProtein={this.props.selectedProtein}
+                    selectedPeptide={this.props.selectedPeptide}
+                    selectedScan={this.props.selectedScan}
+                    selectedPTMPlacement={this.props.selectedPTMPlacement}/>
+                )
+              }.bind(this))
+            }
+          </ul>
         </div>
       )
     } else {
       return (
-        <li className="peptide" onClick={this.toggle}><span className={selected ? 'selectedListItem' : 'unselectedListItem'}>{sequenceName}</span></li>
+        <li className="peptide" onClick={this.toggle}>
+          <span className={selected ? 'selectedListItem' : 'unselectedListItem'}>{sequenceName}</span>
+        </li>
       )
     }
   }
 });
 
 var ProteinListItem = React.createClass({
-  getInitialState: function(){
-    return {open: false,
-            selected: false}
+  getInitialState: function() {
+    return {
+      open: this.childSelected(),
+      selected: false
+    }
   },
+
+  childSelected: function() {
+    return (
+      this.props.selectedProtein == this.props.protein.proteinId &&
+      this.props.selectedPeptide != null
+    )
+  },
+
   defaultProps: function() {
-    return { protein: null,
-             peptide: null,
-             scan: null,
-             selectedProtein: null,
-             selectedPeptide: null,
-             selectedScan: null}
+    return {
+      protein: null,
+      peptide: null,
+      scan: null,
+      selectedProtein: null,
+      selectedPeptide: null,
+      selectedScan: null
+    }
   },
-  toggle: function(){
+
+  toggle: function() {
     this.props.update(this.props.protein.proteinId, null, null, null)
     this.setState({open: !this.state.open})
   },
+
   render: function() {
     var selected = (this.props.selectedProtein == this.props.protein.proteinId &&
                     this.props.selectedPeptide == null &&
                     this.props.selectedScan == null)
-    if (this.state.open){
+    if (this.state.open || this.childSelected()) {
       return (
         <div>
-        <li className="protein" onClick={this.toggle}><span className={selected ? 'selectedListItem' : 'unselectedListItem'}>{this.props.protein.proteinName}</span></li>
-        <ul>
-          { this.props.protein.peptides.map(function(peptide){
-            return ( <PeptideListItem key={[peptide.peptideId, peptide.modificationStateId]}
-                                      proteinId={this.props.protein.proteinId}
-                                      peptideId={peptide.peptideId}
-                                      modificationStateId={peptide.modificationStateId}
-                                      peptideData={this.props.peptideData[peptide.peptideDataId]}
-                                      scans={peptide.scans}
-                                      update={this.props.update}
-                                      selectedProtein={this.props.selectedProtein}
-                                      selectedPeptide={this.props.selectedPeptide}
-                                      selectedPTMPlacement={this.props.selectedPTMPlacement}
-                                      selectedScan={this.props.selectedScan}/>
-            )
-          }.bind(this))}
-        </ul>
+          <li className="protein" onClick={this.toggle}>
+            <span className={selected ? 'selectedListItem' : 'unselectedListItem'}>
+              {this.props.protein.proteinName}
+            </span>
+          </li>
+          <ul>
+            {
+              this.props.protein.peptides.map(function(peptide) {
+                return (
+                  <PeptideListItem
+                    key={[peptide.peptideId, peptide.modificationStateId]}
+                    proteinId={this.props.protein.proteinId}
+                    peptideId={peptide.peptideId}
+                    modificationStateId={peptide.modificationStateId}
+                    peptideData={this.props.peptideData[peptide.peptideDataId]}
+                    scans={peptide.scans}
+                    update={this.props.update}
+                    selectedProtein={this.props.selectedProtein}
+                    selectedPeptide={this.props.selectedPeptide}
+                    selectedPTMPlacement={this.props.selectedPTMPlacement}
+                    selectedScan={this.props.selectedScan}/>
+                )
+              }.bind(this))
+            }
+          </ul>
         </div>
       );
     } else {
       return (
-        <li className="protein" onClick={this.toggle}><span className={selected ? 'selectedListItem' : 'unselectedListItem'}>{this.props.protein.proteinName}</span></li>
+        <li className="protein" onClick={this.toggle}>
+          <span className={selected ? 'selectedListItem' : 'unselectedListItem'}>
+            {this.props.protein.proteinName}
+          </span>
+        </li>
       );
     }
   }
 });
 
 var ScanSelectionList = React.createClass({
-  update: function(proteinId, peptideId, scanId, modsId){
+  update: function(proteinId, peptideId, scanId, modsId) {
     // console.log("proteinId: " + proteinId + " peptideId: " + peptideId + " scanId: " + scanId + " modsId: " + modsId)
     this.props.updateAllCallback(proteinId, peptideId, scanId, modsId);
   },
@@ -295,22 +384,28 @@ var ScanSelectionList = React.createClass({
 
     while (node.length < 4) { node.push(null); }
     this.update(node[0], node[1], node[2], node[3]);
+
+    // TODO: Expand the tree as needed
   },
 
   render: function() {
-    return ( <ul className="tree">
-               {this.props.data.map(function(protein) {
-                  return ( <ProteinListItem key={protein.proteinId}
-                                           protein={protein}
-                                           update={this.update}
-                                           selectedProtein={this.props.selectedProtein}
-                                           selectedPeptide={this.props.selectedPeptide}
-                                           selectedScan={this.props.selectedScan}
-                                           selectedPTMPlacement={this.props.selectedPTMPlacement}
-                                           peptideData={this.props.peptideData}/>
-                         )
-               }.bind(this))}
-             </ul>
-           )
+    return (
+      <ul className="tree">
+        {
+          this.props.data.map(function(protein) {
+            return (
+              <ProteinListItem
+                key={protein.proteinId}
+                protein={protein}
+                update={this.update}
+                selectedProtein={this.props.selectedProtein}
+                selectedPeptide={this.props.selectedPeptide}
+                selectedScan={this.props.selectedScan}
+                selectedPTMPlacement={this.props.selectedPTMPlacement}
+                peptideData={this.props.peptideData}/>
+          )
+        }.bind(this))}
+      </ul>
+    )
   }
 });
