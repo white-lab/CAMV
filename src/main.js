e10c17d8f0980032ef751e7291c92d259f73bb72
Nader Morshed <morshed.nader@gmail.com>
2017-02-23 14:46:14 -0500

    Indentation changes

--- src/main.js
+++ src/main.js
@@ -13,7 +13,10 @@ app.on('window-all-closed', function() {
 });
 
 app.on('ready', function() {
-  mainWindow = new BrowserWindow({width: 1200, height: 600});
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 600
+  });
   mainWindow.loadURL('file://' + __dirname + '/index.html');
   // mainWindow.webContents.openDevTools();
   mainWindow.on('closed', function() {
