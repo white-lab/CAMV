3ef86cdec1a3bd7a99a0b7a29a745f3d7223a0a8
Nader Morshed <morshed.nader@gmail.com>
2017-03-17 15:51:04 -0400

    Only enable hot reload / dev tools in dev env

--- src/main.js
+++ src/main.js
@@ -1,39 +1,50 @@
-const electron = require('electron');
-const {app} = electron;
-const {BrowserWindow} = electron;
-import {enableLiveReload} from 'electron-compile';
+const electron = require('electron')
+const {app} = electron
+const {BrowserWindow} = electron
+import {enableLiveReload} from 'electron-compile'
 
-enableLiveReload();
+if (process.env.NODE_ENV === 'development') {
+  enableLiveReload()
+}
 
-var mainWindow = null;
+var mainWindow = null
 
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1200,
     height: 600,
     icon: __dirname + '../assets/CAMV.ico'
-  });
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
-  // mainWindow.webContents.openDevTools();
-  mainWindow.setMenu(null)
+  })
+
+  mainWindow.loadURL('file://' + __dirname + '/index.html')
+
+  if (process.env.NODE_ENV === 'development') {
+    mainWindow.webContents.openDevTools()
+  } else {
+    mainWindow.setMenu(null)
+  }
+
   mainWindow.on('closed', function() {
-    mainWindow = null;
-  });
+    mainWindow = null
+  })
 }
 
 app.on('window-all-closed', function() {
   if (process.plaform !== 'darwin') {
-    app.quit();
+    app.quit()
   }
-});
+})
 
 app.on('ready', function() {
-  enableLiveReload({strategy: 'react-hmr'});
-  createWindow();
-});
+  if (process.env.NODE_ENV === 'development') {
+    enableLiveReload({strategy: 'react-hmr'})
+  }
+
+  createWindow()
+})
 
 app.on('activate', function() {
-    if (mainWindow === null) {
-        createWindow();
-    }
+  if (mainWindow === null) {
+    createWindow()
+  }
 })
