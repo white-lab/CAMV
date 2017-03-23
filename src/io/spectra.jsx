b6a9283f1d7a210c789ff8408beed46aeafa1030
Nader Morshed <morshed.nader@gmail.com>
2017-03-23 18:33:13 -0400

    Call updateAll directly

--- src/io/spectra.jsx
+++ src/io/spectra.jsx
@@ -45,7 +45,7 @@ exports.spectraToImage = async function(vb, dirName, export_spectras) {
   ]
 
   /* Dummy call to force interface to redraw */
-  vb.refs["scanSelectionList"].update([null, null, null, null])
+  await vb.updateAll([null, null, null, null])
 
   await domtoimage.toPng(
     document.getElementById('viewBox'),
@@ -58,7 +58,7 @@ exports.spectraToImage = async function(vb, dirName, export_spectras) {
     let [nodes, prot, pep, scan, score, state] = vals
     let out_name = prot + " - " + pep + " - " + scan
 
-    await vb.refs["scanSelectionList"].update(nodes)
+    await vb.updateAll(nodes)
 
     // let dataUrl = await domtoimage.toSvg(
     let dataUrl = await domtoimage.toPng(
