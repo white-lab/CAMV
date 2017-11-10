9e3828f9a1672b1d257e0d1b8c883d2c79b2d092
Nader Morshed <morshed.nader@gmail.com>
2017-11-10 12:58:35 -0500

    One more backslash

--- src/io/csv.jsx
+++ src/io/csv.jsx
@@ -94,7 +94,7 @@ exports.exportCSV = async function(vb, path) {
         }
       )
 
-      let new_name = row.name.replace("y", "[pY]") \
+      let new_name = row.name.replace("y", "[pY]")
         .replace("t", "[pT]")
         .replace("s", "[pS]")
         .replace("m", "[oxM]")
